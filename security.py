f28f5d236db635680ddf1b624591ef33ef98dbb7
mgraneros <martin_graneros@hotmail.com>
2026-09-02 20:10:08 -0300

    fix: unificar puerto 465 para envios SSL

--- security.py
+++ security.py
@@ -30,10 +30,10 @@ mail_config = ConnectionConfig(
     MAIL_USERNAME=os.getenv("SMTP_USER", "tu_correo@gmail.com"),
     MAIL_PASSWORD=os.getenv("SMTP_PASSWORD", "tu_contraseña_de_aplicacion"),
     MAIL_FROM=os.getenv("SMTP_FROM", "tu_correo@gmail.com"),
-    MAIL_PORT=int(os.getenv("SMTP_PORT", 587)),
+    MAIL_PORT=int(os.getenv("SMTP_PORT", 465)),
     MAIL_SERVER=os.getenv("SMTP_HOST", "smtp.gmail.com"),
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
